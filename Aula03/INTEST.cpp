44454e830e00037533516416f177fdd91125153d
Sel <38021042+seleneal1996@users.noreply.github.com>
2021-09-23 04:33:49 +0000

    add INTEST - Enormous Input Test

--- Aula03/INTEST.cpp
+++ Aula03/INTEST.cpp
@@ -1,1 +1,17 @@
 //INTEST - Enormous Input Test
+#include <iostream>
+using namespace std;
+int main(){
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  int m,l,y;
+  int cont=0;
+  cin>>m>>l;
+
+  while(n--){
+    cin>>y;
+    cont=(x%k==0)?cont+1:cont;
+  }
+  cout<<cont<<"\n";
+}
