9e844f9d241a71380d8dc5666476a9dbf0af3975
Sel <38021042+seleneal1996@users.noreply.github.com>
2021-09-24 04:02:32 +0000

    add Subarray sum equals k

--- Aula03/560.cpp
+++ Aula03/560.cpp
@@ -5,11 +5,25 @@ using namespace std;
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        
+    int n=nums.size();
+    int count=0;
+    for (int i=0; i<n;i++){
+      for(int j=i+1;j<=n;j++){
+          int sum=0;
+          for(int ki=i;ki<j;ki++){
+              sum+=nums[ki];
+          if(sum==k)
+              count++;
+          
+          }
+      }
+    return count;
+    }
+   
     }
 };
-
-int main()
-{
-  
+int main(){
+    vector<int> arreglo_1={1,1,1};
+    Solution S1=Solution();
+    cout<<S1.subarraySum(arreglo_1,2)<<endl;
 }
