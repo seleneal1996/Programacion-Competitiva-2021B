ad78bbee091f1d9fb46425ae69d3b86b1e69eae6
Sel <38021042+seleneal1996@users.noreply.github.com>
2021-09-27 17:21:05 +0000

    Update

--- Aula04/INTEST.cpp
+++ Aula04/INTEST.cpp
@@ -1,7 +1,10 @@
 //INTEST - Enormous Input Test
+/*cabecera*/
 #include <iostream>
+#include<bits/stdc++.h> //libreria STL
 using namespace std;
 int main(){
+  //Leer entrada y salida de forma rapida
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
 
