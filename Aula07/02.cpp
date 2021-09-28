a10916e50bfb2ca0d310df54208884b77a7f19e1
Sel <38021042+seleneal1996@users.noreply.github.com>
2021-09-28 23:48:20 +0000

    Add Aula07 and Update Aula06

---
+++ Aula07/02.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include<bits/stdc++.h> //libreria STL
+using namespace std;
+
+
+int main()
+{
+   //Leer entrada y salida de forma rapida
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+	
+	return 0;
+}
