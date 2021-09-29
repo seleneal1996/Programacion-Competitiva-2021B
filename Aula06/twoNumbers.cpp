2059dd5e1f83666fc65171808ad6cd11ee9bd655
Sel <38021042+seleneal1996@users.noreply.github.com>
2021-09-29 00:35:41 +0000

    Add Problem 01 - AULA06

--- Aula06/twoNumbers.cpp
+++ Aula06/twoNumbers.cpp
@@ -1,36 +1,49 @@
 #include <iostream>
 #include<bits/stdc++.h> //libreria STL
 using namespace std;
-class solution{
+class solution
+{
     public:
-    vector<int>twoNUmbers(vector<int>& numbers, int target){
-        vector<int> solu;
-    	for (int i = 0; i < numbers.size() ; i++)
-    	{
-    		for(int j =i + 1; j < numbers.size(); j++)
-    		{
-    		    if(numbers[j]==target-numbers[i]){
-    		        solu.push_back(i+1);
-    		        solu.push_back(j+1);
-    		        if(numbers[i]<numbers[j]){
-    		            i++;
-    		            j++;
-    		        }
-    		    }
-    		}
-    	}
-    	return solu; 
+    vector<int>findtwoNUmbers(vector<int>& numbers, int target)
+    {
+      int i=0,j=numbers.size()-1, tmp1=0,tmp2=0,k=0;
+      vector<int> solu(numbers.size());
+      for(int l=0;l<numbers.size();l++){
+        solu[k++]=numbers[l];
       }
+      sort(numbers.begin(), numbers.end());
+      while(i<j)
+      {
+        if(solu[i]+solu[j]==target)
+        {
+          tmp1=solu[i];
+          tmp2=solu[j];
+        }
+        if(solu[i]+solu[j]<target)
+          i++;
+        else{
+          j--;
+        }
+      }
+      k=0;
+      vector<int> res(2);;
+      for(int l=0;l<numbers.size();l++){
+          if(tmp1==numbers[l] || tmp2==numbers[l]){
+              res[k++]=l;
+          }
+      }
+      return res;
+    } 
 };
 
 int main()
 {
    //Leer entrada y salida de forma rapida
-  //ios_base::sync_with_stdio(false);
-  //cin.tie(NULL);
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  vector<int> nums={2,7,11,15};
+  int target=9;
   solution S1=solution();
-  vector<int> arr = {2,7,11,15 };
-  int target = 9;
-  S1.twoNUmbers(arr, target);
+  S1.findtwoNUmbers(nums,target);
 }
   
