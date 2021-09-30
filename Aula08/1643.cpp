e37612a9d735186cf654f4e966c91fa34230b9d2
Sel <38021042+seleneal1996@users.noreply.github.com>
2021-09-30 20:08:31 +0000

    add 1643.cpp

--- Aula08/1643.cpp
+++ Aula08/1643.cpp
@@ -7,6 +7,23 @@ int main()
    //Leer entrada y salida de forma rapida
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
-	
+  //Declaramos longitud de matrix
+	int n;
+  //declaramos la matrx valor
+  vector<long long > valor(n);
+  // el long long para evitar problema de desbordamiento
+  long long b= -1e18;
+  long long p= 0;
+  for(int i=0;i<n;i++)
+  {
+    if(p+valor[i>=valor[i]]){
+      p+=valor[i];
+    }
+    else{
+      p=valor[i];
+    }
+    b=max(b,p);
+  }
+  cout<<b;
 	return 0;
 }
