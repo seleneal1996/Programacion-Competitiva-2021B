b278cbf70e7a4c834725aecac446b9a5d2e5f54d
Sel <38021042+seleneal1996@users.noreply.github.com>
2021-10-10 21:16:03 +0000

    add Q10

--- EX1/CODFURY_Megatron_and_his_rage.cpp
+++ EX1/CODFURY_Megatron_and_his_rage.cpp
@@ -1,44 +1,46 @@
-#include <iostream>
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
+#define MAX 50000
 typedef long long ll;
-int main() {
-	
-	ll B[INT_MAX]; 
-  ll P,M;
-  int T;
-	std::cin>>T;
-	do{
-		std::cin>>P>>M;
-		for(int i=1;i<=P;i++){
-			std::cin>>B[i];
-		}
-		
-		ll Pminsumx=50000;
-		int ini=1;
-		ll mx_st=0;
-		ll state=0;
-		ll sumx=0;
-		
-		for(int i=1;i<=P;i++){
-			sumx=sumx+B[i];
-			state++;
-			if((mx_st<state&&sumx<=M)||(mx_st==state&&Pminsumx>sumx)){
-				mx_st=state;
-				Pminsumx=sumx;
-			}
-			while(sumx>M){
-				sumx=sumx-B[ini];
-				state--;
-				ini++;
-			}
-			if((mx_st<state &&sumx<=M)||(mx_st==state&&Pminsumx>sumx)){
-				mx_st=state;
-				Pminsumx=sumx;
-			}
-		}
-		std::cout<<Pminsumx<<mx_st;
-	}
-	while(T--);
-	return 0;
+ll B[MAX];
+int main()
+{
+    int l, T, ini;
+    ll sumx, min_sumx, state, mx_state;
+    ll P, M;
+    std::cin>>T;
+
+    while(T--)
+    {
+        std::cin>>P>>M;
+        for(int i=1; i<=P; i++)
+          std::cin>>B[i];
+        min_sumx = 50000;
+        ini = 1;
+        mx_state = 0;
+        state=0;
+        sumx = 0;
+        for(int i=1; i<=P; i++)
+        {
+          sumx = sumx + B[i];
+          state = state + 1;
+          if((mx_state < state && sumx <=M) || (mx_state == state && min_sumx > sumx))
+          {
+            mx_state = state;
+            min_sumx = sumx;
+          }
+          while(sumx>M)
+          {
+            sumx = sumx - B[ini];
+            state--;
+            ini++;
+          }
+          if ((mx_state < state && sumx <=M) || (mx_state == state && min_sumx > sumx))
+          {
+            mx_state = state;
+            min_sumx = sumx;
+          }
+     }
+    std::cout<<min_sumx<<mx_state<<endl;
+    }
 }
