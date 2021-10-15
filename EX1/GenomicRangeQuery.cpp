fbb5b0cac6824abc83c525a53ee3d4028ca620e5
Sel <38021042+seleneal1996@users.noreply.github.com>
2021-10-15 12:18:36 -0500

    Update GenomicRangeQuery.cpp
    Update Q2: 62%

--- EX1/GenomicRangeQuery.cpp
+++ EX1/GenomicRangeQuery.cpp
@@ -4,52 +4,53 @@
 // you can write to stdout for debugging purposes, e.g.
 // cout << "this is a debug message" << endl;
 
-std::vector<int> solution(std::string &S,std::vector<int> &P, std::vector<int> &Q) {
-    // write your code in C++14 (g++ 6.2.0)
-    int n=S.length();
-    int m=P.size();
-    std::vector<int>resultado(m);
-    int c[4][n];
-    int prefixSum[4][n+1];
-    for (int i = 0; i <n; i++) 
+vector<int> solution(string &S, vector<int> &P, vector<int> &Q) {
+	int n = S.length();
+    int M = P.size();
+	vector<int> result(M, 0);
+    vector<int> V(n, 0);
+    const int A = 1;
+	const int C = 2;
+	const int G = 3;
+	const int T = 4;
+	for (int i=0; i<n; i++)
     {
-        char ch = S.at(i);
-        switch(ch)
-        {
-            case 'A':
-                c[0][i]++;
+		switch (S[i]) {
+			case 'A': 
+                V[i] = A; 
                 break;
-            case 'C':
-                c[1][i]++;
+			case 'C': 
+                V[i] = C; 
                 break;
-            case 'G':
-                c[2][i]++;
+			case 'G': 
+                V[i] = G; 
                 break;
-            case 'T':
-                c[3][i]++;
+			case 'T': 
+                V[i] = T; 
                 break;
-        }
-    }
-    for (int k = 1; k < n + 1; k++) 
-    {
-        for (int j = 0; j < 4; j++)
-        {
-            prefixSum[j][k] = prefixSum[j][k-1] + c[j][k-1];
-        }
-    }
+			default: 
+                break;
+		}
+	}
 
-    for (int i = 0; i < m; i++)
-    {
-        int x = P[i];
-        int y = Q[i];
-        for (int j = 0; j < 4; j++)
-        {
-            if (prefixSum[j][y + 1] - prefixSum[j][x] > 0)
-            {
-                resultado[i] = j + 1;
-                break;
-            }
-        }
-    }
-    return resultado;
+	for (int m=0; m<M; m++) {
+		int l = P[m];
+		int r = Q[m];
+
+		int minimum = 4;
+
+		for (int i=l; i<=r; i++) {
+			if (V[i] < minimum) {
+				minimum = V[i];
+			}
+			
+			if (minimum == 1) {
+				result[m] = minimum;
+				break;
+			}
+		}
+		result[m] = minimum;
+	}
+
+	return result;
 }
