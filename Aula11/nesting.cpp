511e786ae71f856e113860d1a41750555971f10c
Sel <38021042+seleneal1996@users.noreply.github.com>
2021-10-21 23:41:25 +0000

    Update Q1. Brackets - status : accepted

--- Aula11/nesting.cpp
+++ Aula11/nesting.cpp
@@ -3,19 +3,36 @@
 
 // you can write to stdout for debugging purposes, e.g.
 // cout << "this is a debug message" << endl;
-#include <bits/stdc++. h>
-int solution(string &s) {
-    stack<char>A;
-    int n=s.size();
-    for (int i=0;i<n;i++){ 
-        if(A.top()=='{' || s[i]=='[' || s[i]=='('){
-            A.push(s[i]);
-        }
-        else{
-            if(A.empty()){
-                return 0;
-            }
-        }
+#include <bits/stdc++.h>
+int solution(string &S) {
+    vector<int> A;
+    int n=S.size(),cont;
+    if(S.empty()){
+        return 1;
     }
-    return 1;
+    for(int i=0;i<n;i++){
+        if(S[i]=='['){
+            cont=+1;
+        }
+        if(S[i]==']'){
+            cont=-1;
+        }
+        if(S[i]=='('){
+            cont=+2;
+        }
+        if(S[i]==')'){
+            cont=-2;
+        }
+        if(S[i]=='{'){
+            cont=+3;
+        }
+        if(S[i]=='}'){
+            cont=-3;
+        }
+        if((i==0 && cont<0) || (cont<0 && cont!=-A.back())){return 0;}
+        else if(cont<0){A.pop_back();}
+        else{A.push_back(cont);}
+    }
+    if(A.empty()){return 1;}
+    else {return 0;}
 }
