e066ae9fafafbdae99e8c30963a85903fbda1d17
Sel <38021042+seleneal1996@users.noreply.github.com>
2021-10-25 13:48:18 +0000

    incomplete aula12

--- Aula11/1566: Height/1566.cpp
+++ Aula11/1566: Height/1566.cpp
@@ -1,22 +1,39 @@
 #include <bits/stdc++.h>
-#define MAX  3000000
+#define maximo 3000000
+void ordenacao(int vetor[],long long int tam)
+{
+    long long int i,j,aux;
+    for(i=0;i<tam;i++)
+        {
+        for(j=0;j<tam;j++)
+            {
+                if(vetor[i]<vetor[j])
+                    {
+                        aux=vetor[i];
+                        vetor[i]=vetor[j];
+                        vetor[j]=aux;
+                    }
+            }
+        }
+}
 int main()
 {
-  std::ios_base::sync_with_stdio(false);
-  std::cin.tie(NULL);
-  int array[MAX]={0};
-  int tc,n;
-  std::cin>>tc;
-  while(tc--){
-    std::cin>>n;
-    for(int j=0;j<n;j++){
-      std::cin>>array[j];
+  long long int M,i,p,j;
+  int k,test,aux;
+  int *v;
+  v = (int *) malloc(maximo);
+  scanf("%d",&test);
+  for(k=0;k<test;k++){
+    scanf("%lld",&M);
+    for(p=0;p<M;p++){
+      scanf("%d",&v[p]);
+      ordenacao(v,M);
+      printf("%d",v[0]);
     }
-    std::sort(array,array+n);
-    for(int i=0;i<n-1;i++)
-      std::cout<<array[i];
-    std::cout<<array[n-1];
-    std::cout<<"\n";
+    
+    for(i=1;i<M;i++)
+        printf(" %d",v[i]);
+     printf("\n");
   }
   return 0;
 }
---
+++ Aula12/capsLock.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+void Solution(std::string cadena){
+  std::queue<char> teclado;
+  std:: string f="";
+  int n= cadena.length();
+  bool c= true;
+
+  for(int pos =0;pos<n;pos++){
+    if(cadena[i]=='@'){
+      if(c==true){
+        
+      }
+    }
+  }
+}
+int main(){
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(NULL);
+}
