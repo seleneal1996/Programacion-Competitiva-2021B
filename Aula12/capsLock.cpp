39819b9c0fe3087198f2e6f4c54a7934e37acdcb
Sel <38021042+seleneal1996@users.noreply.github.com>
2021-10-27 03:21:31 +0000

    UPDATE Q1: Caps Lock :D

--- Aula12/capsLock.cpp
+++ Aula12/capsLock.cpp
@@ -3,17 +3,35 @@ void Solution(std::string cadena){
   std::queue<char> teclado;
   std:: string f="";
   int n= cadena.length();
-  bool c= true;
+  bool c= false;
 
-  for(int pos =0;pos<n;pos++){
-    if(cadena[i]=='@'){
-      if(c==true){
-        
+  for(int pos =0;pos<n;pos++)
+  {
+    if(cadena[pos]=='@'){
+      if(c){
+        c=true;
+      }
+      else if(cadena[pos]=='$'){
+        while(!teclado.empty()){
+          if(c){
+            f+=toupper(teclado.front());
+          }
+          else{
+            f+=tolower(teclado.front());
+          }
+        }
+      }
+      else{
+        teclado.push(cadena[pos]);
       }
     }
   }
+  std::cout<<f<<std::endl;
 }
 int main(){
   std::ios_base::sync_with_stdio(false);
   std::cin.tie(NULL);
+  std::string cadena;
+  std::cin>>cadena;
+  Solution(cadena);
 }
