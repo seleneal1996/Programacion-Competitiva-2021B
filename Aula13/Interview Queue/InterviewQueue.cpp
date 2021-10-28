1e52f66aceb0ea6f1710b7989186f08b1c02c068
Sel <38021042+seleneal1996@users.noreply.github.com>
2021-10-28 18:19:44 +0000

    add dole

--- Aula13/Interview Queue/InterviewQueue.cpp
+++ Aula13/Interview Queue/InterviewQueue.cpp
@@ -1,10 +1,9 @@
 #include <bits/stdc++.h>
 
-
 int main()
 {
-  std::ios_base::sync_with_stdio(false);
+  std::ios::sync_with_stdio(false);
   std::cin.tie(NULL);
-  
-  
+
+  return 0;
 }
--- Aula13/The Dole Queue/dole.cpp
+++ Aula13/The Dole Queue/dole.cpp
@@ -1,10 +1,81 @@
 #include <bits/stdc++.h>
-
-
-int main()
+int n,k,m,j,der = 0,cont = 0,sOff = 0;
+int izq = n-1;
+int main () 
 {
-  std::ios_base::sync_with_stdio(false);
-  std::cin.tie(NULL);
-  
-  
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    bool A[20];
+    while (std::cin >> n >> k >> m)
+    {
+      if (n == 0 && k == 0 && m == 0)
+      {
+        return 0;
+      }
+      for (int i = 0; i < 20; i++)
+      {
+        A[i] = false;
+      }
+      for (int i = 0; i < n; i++)
+      {
+        A[i] = true;
+      }
+      while (sOff < n)
+      {
+        j = der;
+        cont = 0;
+        while (cont < m) 
+        {
+          if (j == 0)
+          {
+            j = n - 1;
+          }
+          else
+          {
+            --j;
+          }
+          if (A[j] == true)
+          {
+            cont++;
+          }
+          der = j;
+          j = izq;
+          cont = 0;
+          while (cont < k)
+          {
+            if (j == n-1)
+            {
+              j = 0;
+            }
+            else
+            {
+              ++j;
+            }
+            if (A[j] == true)
+            {
+              cont++;
+            }
+          }
+          
+          izq = j;
+          if (sOff > 0){
+            std::cout << ",";
+          }
+          if (izq == der){
+            /*Establece el ancho del campo que se utilizará en las operaciones de salida.*/
+            std::cout << std::setw(3) << izq + 1;
+            sOff += 1;
+          } 
+          else
+          {
+            std::cout << std::setw(3) << izq + 1 << std::setw(3) << der + 1;
+            sOff += 2;
+        }
+        A[izq] = false;
+        A[der] = false;
+        }
+        std::cout << '\n';
+        }
+    return 0;
+    }
 }
