c1712dfbb0b93ec3db9c6d95a58252765d10210f
Sel <38021042+seleneal1996@users.noreply.github.com>
2021-11-10 05:41:42 +0000

    add Folder: Aula 16

---
+++ Aula16/10 Kinds of People/10People.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+class Solution {
+public:
+
+};
+int main(){
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(NULL);
+  Solution S1= Solution();
+  //caso de prueba
+  return 0;
+}
