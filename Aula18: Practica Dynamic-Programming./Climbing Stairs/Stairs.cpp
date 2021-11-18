2ff23c08994861a54ba2b7557175eb0e44e3b914
Sel <38021042+seleneal1996@users.noreply.github.com>
2021-11-18 17:55:03 +0000

    add stairs.cpp

--- Aula18: Practica Dynamic-Programming./Climbing Stairs/Stairs.cpp
+++ Aula18: Practica Dynamic-Programming./Climbing Stairs/Stairs.cpp
@@ -1,11 +1,24 @@
 #include <bits/stdc++.h>
 class Solution {
-  public:
-
+public:
+    int climbStairs(int n) {
+        if(n==1)
+            return 1;
+        int first=1;
+        int second=2;
+        for(int i=3;i<=n;i++){
+            int third=second+first;
+            first=second;
+            second=third;
+        }
+        return second;
+    }
 };
 int main(){
   std::ios_base::sync_with_stdio(false);
   std::cin.tie(NULL);
   Solution S1= Solution();
+  //caso de prueba
+  S1.climbStairs(2);
   return 0;
 }
