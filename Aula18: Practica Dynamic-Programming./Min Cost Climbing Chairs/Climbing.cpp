9c098748bebcd11867cf2ac3bf55d90e60bc2b48
Sel <38021042+seleneal1996@users.noreply.github.com>
2021-11-18 18:41:58 +0000

    add climbing.cpp

--- Aula18: Practica Dynamic-Programming./Min Cost Climbing Chairs/Climbing.cpp
+++ Aula18: Practica Dynamic-Programming./Min Cost Climbing Chairs/Climbing.cpp
@@ -1,11 +1,19 @@
 #include <bits/stdc++.h>
 class Solution {
-  public:
-
+public:
+    int minCostClimbingStairs(std::vector<int>& cost) {
+        int n=cost.size() ;
+        for (int i =n-3; ~i; i--)
+            cost[i] += fmin(cost[i+1], cost[i+2]);
+        return fmin(cost[0], cost[1]);
+    }
 };
 int main(){
   std::ios_base::sync_with_stdio(false);
   std::cin.tie(NULL);
   Solution S1= Solution();
+  //caso de prueba [10,15,20]
+  std::vector<int>cost={10,15,20};
+  std::cout<<S1.minCostClimbingStairs(cost);
   return 0;
 }
