512f66da02b4f80de400378be56d06c1beea3d75
Sel <38021042+seleneal1996@users.noreply.github.com>
2021-11-18 16:53:56 +0000

    Add rings

--- Aula16/Rings/rings.cpp
+++ Aula16/Rings/rings.cpp
@@ -1,12 +1,84 @@
 #include <bits/stdc++.h>
 class Solution {
-public:
+  public:
+  int h, w;
+  void Solve()
+  {
+    std::cin >> h >> w;
+    char T[150][150];
+    for(int i = 0; i < 103; i++) 
+    {
+      for(int j = 0; j < 103; j++)
+      {
+        T[i][j] = '.';
+      }
+    }
+
+    for(int i = 1; i <= h; i++)
+    {
+      for(int j = 1; j <= w; j++){
+        std::cin >> T[i][j];
+      }
+    }
+
+    bool intercambio = true;
+    int k = 0;
+    while(intercambio)
+    {
+      k++;
+      intercambio = false;
+      for(int i = 1; i <= h; i++)
+      {
+        for(int j = 1; j <= w; j++)
+        {
+          if(T[i][j] == 'T' && (T[i-1][j] == '.' || T[i+1][j] == '.' || T[i][j-1] == '.' || T[i][j+1] == '.'))
+          {
+            T[i][j] = k + '0';
+            intercambio = true;
+          }
+          if(T[i][j] == 'T' && (T[i-1][j] == k + '0' - 1 || T[i+1][j] == k + '0' - 1 || T[i][j-1] == k + '0' - 1 || T[i][j+1] == k + '0' - 1)) 
+          {
+            T[i][j] = k + '0';
+            intercambio = true;
+          }
+
+        }
+      }
+    }
+
+    for(int i = 1; i <= h; i++)
+    {
+      for(int j = 1; j <= w; j++)
+      {
+        if(k < 10)
+        {
+          if(T[i][j] == '.')
+            std::cout << "..";
+          else
+            std::cout << '.' << int(T[i][j] - '0');
+        }
+        else
+        {
+          if(T[i][j] == '.')
+            std::cout << "...";
+          else
+          {
+            if(int(T[i][j] - '0') >= 10)
+              std::cout << '.' << int(T[i][j] - '0');
+            else
+              std::cout << ".." << int(T[i][j] - '0');
+          }
+        }
+     }
+     std::cout << '\n';
+    }
+}
 
 };
+
 int main(){
   std::ios_base::sync_with_stdio(false);
   std::cin.tie(NULL);
   Solution S1= Solution();
-  //caso de prueba
-  return 0;
+  S1.Solve();
 }
