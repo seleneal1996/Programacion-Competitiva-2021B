abc971fdf1f79ccf949eded0ad7d1d689f5c6aa9
Sel <38021042+seleneal1996@users.noreply.github.com>
2021-11-19 12:28:48 -0500

    Update tilin.cpp

--- Aula18: Practica Dynamic-Programming./Tiling/tilin.cpp
+++ Aula18: Practica Dynamic-Programming./Tiling/tilin.cpp
@@ -4,7 +4,7 @@ class Solution {
   int tilin(int n){
     if (n <= 1) 
       return n;
-    vi dp(n+1, 0);
+    std::vector<int> dp(n+1, 0);
     dp[0] = 0;
     dp[1] = 1;
     for (int i = 2; i <= n; i+=1){
