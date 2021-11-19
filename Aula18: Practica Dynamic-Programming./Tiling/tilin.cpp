055bd8b98985e315a2a644959ba378c84efdb7a5
Sel <38021042+seleneal1996@users.noreply.github.com>
2021-11-19 12:26:59 -0500

    Update tilin.cpp

--- Aula18: Practica Dynamic-Programming./Tiling/tilin.cpp
+++ Aula18: Practica Dynamic-Programming./Tiling/tilin.cpp
@@ -1,11 +1,26 @@
 #include <bits/stdc++.h>
 class Solution {
   public:
-
+  int tilin(int n){
+    if (n <= 1) 
+      return n;
+    vi dp(n+1, 0);
+    dp[0] = 0;
+    dp[1] = 1;
+    for (int i = 2; i <= n; i+=1){
+      dp[i] = dp[i-1] + dp[i-2];
+    }
+    return dp[n];
+  }
 };
 int main(){
   std::ios_base::sync_with_stdio(false);
   std::cin.tie(NULL);
   Solution S1= Solution();
+  //CASO DE PRUEBA=4
+  //SALIDA=3
+  //CASO DE PRUEBA=8
+  //SALIDA=21
+  sts::cout <<S1.tilin(8) << endl;
   return 0;
 }
