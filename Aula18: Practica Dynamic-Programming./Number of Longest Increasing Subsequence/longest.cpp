99ab1d72983c8cf57b90a0761dd38b1dcd6804b9
Sel <38021042+seleneal1996@users.noreply.github.com>
2021-11-19 03:08:20 +0000

    add longest.cpp

--- Aula18: Practica Dynamic-Programming./Number of Longest Increasing Subsequence/longest.cpp
+++ Aula18: Practica Dynamic-Programming./Number of Longest Increasing Subsequence/longest.cpp
@@ -1,11 +1,31 @@
 #include <bits/stdc++.h>
 class Solution {
-  public:
-
+public:
+    int lengthOfLIS(std::vector<int>& nums) {
+       int n = nums.size();
+       int L[n];
+       L[0] = 1;
+       for(int i=1;i<n;i++){
+           L[i] = 1;
+           for(int j=0;j<i;j++){
+               if(nums[j]<nums[i])
+                L[i] = fmax(L[i],L[j]+1);
+           }
+       }
+       
+       int maxL = L[0];
+       for(int i=1;i<n;i++)
+        maxL = fmax(L[i],maxL);
+       return maxL;
+    }
 };
 int main(){
   std::ios_base::sync_with_stdio(false);
   std::cin.tie(NULL);
   Solution S1= Solution();
+  //CASO DE PRUEBA [10,9,2,5,3,7,101,18]
+  //SALIDA: 4
+  std::vector<int>nums={10,9,2,5,3,7,101,18};
+  std:: cout<<S1.lengthOfLIS(nums);
   return 0;
 }
