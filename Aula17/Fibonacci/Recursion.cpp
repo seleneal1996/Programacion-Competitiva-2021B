85db548e7d7db2bce44d15bb1782bc0418136e19
Sel <38021042+seleneal1996@users.noreply.github.com>
2021-11-18 23:05:51 -0500

    Update Recursion.cpp

--- Aula17/Fibonacci/Recursion.cpp
+++ Aula17/Fibonacci/Recursion.cpp
@@ -1,11 +1,19 @@
 #include <bits/stdc++.h>
 class Solution {
   public:
-
+  long fib_recursivo(int n)
+  {
+    if(n <= 1)
+      return n;
+    return fib(n-1) + fib(n-2);
+  }
 };
 int main(){
   std::ios_base::sync_with_stdio(false);
   std::cin.tie(NULL);
   Solution S1= Solution();
+  //CASO DE PRUEBA: 2
+  //SALIDA:1
+  std::cout<<S1.fib_recursivo(2);
   return 0;
 }
