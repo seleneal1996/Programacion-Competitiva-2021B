e9cb7fa493082b5079c8675ef50d713caf7a4445
Sel <38021042+seleneal1996@users.noreply.github.com>
2021-11-21 00:01:07 -0500

    Update unique.cpp

--- Aula19: Practica Practica_18-nov/8. Unique Paths/unique.cpp
+++ Aula19: Practica Practica_18-nov/8. Unique Paths/unique.cpp
@@ -1,7 +1,29 @@
 //https://leetcode.com/problems/unique-paths/
+#include <bits/stdc++.h>
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        
+        std::vector<std::vector<int>> dp(m, std::vector<int>(n));
+        for(int i = 0; i < m; ++i){
+            for(int j = 0; j < n; ++j){
+                if(i == 0 || j == 0)
+                dp[i][j] = 1;
+            }
+        }
+        for(int i = 1; i < m; ++i){
+            for(int j = 1; j < n; ++j){
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
+int main(){
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(NULL);
+  Solution S1= Solution();
+  //caso de entrada 3,7
+  //salida: 28
+  std::cout<<S1.uniquePaths(3,7);
+  return 0;
+}
