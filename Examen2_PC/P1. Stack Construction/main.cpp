e15d93b585221cd374335e0fc7af26011819735d
Sel <38021042+seleneal1996@users.noreply.github.com>
2021-11-25 19:18:55 +0000

    add q1-ex2

---
+++ Examen2_PC/P1. Stack Construction/main.cpp
@@ -0,0 +1,12 @@
+//https://open.kattis.com/problems/stack
+
+#include <iostream>
+class Solution {
+public:
+    
+};
+int main() {
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(NULL);
+  Solution S1= Solution();
+} 
