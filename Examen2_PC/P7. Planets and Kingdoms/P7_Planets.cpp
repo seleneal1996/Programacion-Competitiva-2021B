234f7134c69daa8a9d034e745607cddeb7503e5f
Sel <38021042+seleneal1996@users.noreply.github.com>
2021-11-26 13:30:14 -0500

    Update P7_Planets.cpp

--- Examen2_PC/P7. Planets and Kingdoms/P7_Planets.cpp
+++ Examen2_PC/P7. Planets and Kingdoms/P7_Planets.cpp
@@ -2,86 +2,68 @@
 #include <bits/stdc++.h>
 class P7{
   public:
-  std::vector<bool> visit;
-  std::vector<int> part, sequence,root, base_nodes;
-  std::vector<std::vector<int>> adj, adj_rev;
+  int visit[100005];
+  std::stack<int>A;
+  std::vector<int> part[100005], sequence[100005]/*,root, base_nodes*/;
+  //std::vector<std::vector<int>> adj, adj_rev;
+  int k=0;
   void first_dfs(int u)
   {
-    visit[u] = true;
-    for(auto v: adj[u])
-    {
-        if(!visit[v])
-          first_dfs(v);
-    }
-    sequence.push_back(u);
+    if (visit[u])
+      return;
+    visit[u]=1;
+    for(auto var:part[u])
+      first_dfs(var);
+    A.push(u);
+ 
   }
-
   void second_dfs(int u)
   {
-    visit[u] = true;
-    part.push_back(u);
-    for(auto v: adj_rev[u])
-    {
-      if(!visit[v])
-        second_dfs(v);
-    }
+    if(visit[u])
+      return;
+    visit[u]=k;
+    for(auto var:sequence[u])
+      second_dfs(var);
   }
-
+ 
+  void final_solution(){
+    int n,m;
+    std::cin>>n>>m;
+    for (int i=0; i<m; i++){
+      int a,b;
+      std::cin>>a>>b;
+      part[a].push_back(b);
+      sequence[b].push_back(a);
+    }
+    for (int i=1; i<n+1; i++){
+      if(!visit[i])
+        first_dfs(i);
+    }
+    memset(visit,0,sizeof visit);
+    while(!A.empty())
+    {
+        int x = A.top(); 
+        A.pop();
+        if (!visit[x]) 
+        {
+          k++;
+          second_dfs(x);
+        }
+    }
+    std::cout << k <<"\n";
+    for (int i=1; i<n+1; i++)
+      std::cout <<visit[i]<<"\n";
+  }
+ 
 };
  
 int main(){
     std::ios::sync_with_stdio(0);
     std::cin.tie(0);
     P7 S1= P7();
-    int n, m;
-    std::vector<bool> visit;
-    std::vector<int> part, sequence,root, base_nodes;
-    std::vector<std::vector<int>> adj, adj_rev;
-    std::cin >> n >> m;
-    adj.resize(n);
-    adj_rev.resize(n);
- 
-    for(int i = 0; i < m; i++){
-        int u, v;
-        std::cin >> u >> v;
-        u--, 
-        v--;
-        adj[u].push_back(v);
-        adj_rev[v].push_back(u);
-    }
- 
-    visit.assign(n, false);
- 
-    for(int i = 0; i < n; i++){
-        if(!visit[i]){
-            S1.first_dfs(i);
-        }
-    }
- 
-    root.assign(n, 0);
-    visit.assign(n, false);
-    reverse(sequence.begin(), sequence.end());
-    for(auto u: sequence)
-    {
-      if(!visit[u])
-      {
-        S1.second_dfs(u);
-        int seed = part.front();
-        for (auto u : part) root[u] = seed;
-        base_nodes.push_back(seed);
-        part.clear();
-      }
-    }
- 
-    int cnt = 0;
-    std::map<int, int> key;
-    for(auto e: base_nodes){
-        key[e] = ++cnt;
-    }
- 
-    std::cout << cnt << '\n';
-    for(int i = 0; i < n; i++){
-        std::cout << key[root[i]] << ' ';
+    int t=1;
+    while(t--){
+      S1.final_solution();
     }
     return 0;
 }
