2f007515373c4320e49b2d86ee36232e0d517201
Sel <38021042+seleneal1996@users.noreply.github.com>
2021-12-20 10:08:03 -0500

    Update s24_Many.cpp

--- Aula24:Practica-Mathematics/02How Many Digits?/s24_Many.cpp
+++ Aula24:Practica-Mathematics/02How Many Digits?/s24_Many.cpp
@@ -1,1 +1,16 @@
 //https://open.kattis.com/problems/howmanydigits
+#include <bits/stdc++.h>
+using namespace std;
+int c=1e6;
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    double arr[c];
+    arr[0] = 0;
+    for (int x = 1; x <= 1e6; x++)
+        arr[x] = arr[x - 1] + log10(x);
+    int num;
+    while (cin >> num)
+        cout << (int)arr[num] + 1 << '\n';
+    return 0;
+}
