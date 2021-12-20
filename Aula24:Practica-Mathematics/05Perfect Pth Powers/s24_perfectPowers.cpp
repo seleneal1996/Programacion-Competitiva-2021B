225754d10eaca087de0b459d03a405db49e07143
Sel <38021042+seleneal1996@users.noreply.github.com>
2021-12-20 17:06:40 -0500

    Update s24_perfectPowers.cpp

--- Aula24:Practica-Mathematics/05Perfect Pth Powers/s24_perfectPowers.cpp
+++ Aula24:Practica-Mathematics/05Perfect Pth Powers/s24_perfectPowers.cpp
@@ -1,1 +1,36 @@
 //https://open.kattis.com/problems/perfectpowers
+#include <bits/stdc++.h>
+int main()
+{
+  long long x;
+  const double error = 0.0001;
+  while (true)
+  {
+    std::cin >> x;
+    if (x==0) break;
+    double b;
+    for (long long power=31;power>0;power--)
+    {
+      b = std::pow(std::abs(x),1.0/(double)power);
+      if (std::ceil(b)-b < error)
+      {
+        if (std::pow(std::ceil(b),power)==std::abs(x) && (x>0 || power%2!=0))
+        {
+          std::cout << power << std::endl;
+          break;
+        }
+      }
+      else if (b-std::floor(b)<error)
+      {
+        if (std::pow(std::floor(b),power)==std::abs(x) && (x>0 || power%2!=0))
+        {
+          std::cout << power << std::endl;
+          break;
+        }
+      }
+
+    }
+
+  }
+  return 0;
+}
