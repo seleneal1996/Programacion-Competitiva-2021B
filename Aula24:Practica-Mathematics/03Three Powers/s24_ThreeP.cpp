7119e8c25ac5ccc9b45cee7a50dad209b4fbdb13
Sel <38021042+seleneal1996@users.noreply.github.com>
2021-12-20 16:37:32 -0500

    Update s24_ThreeP.cpp

--- Aula24:Practica-Mathematics/03Three Powers/s24_ThreeP.cpp
+++ Aula24:Practica-Mathematics/03Three Powers/s24_ThreeP.cpp
@@ -1,1 +1,30 @@
-//https://open.kattis.com/problems/threepowers
+  //https://open.kattis.com/problems/threepowers
+  #include <bits/stdc++.h>
+  void print_set(int n)
+  {
+    std::string nums [64]{"1","7","14","783","1125900981634049","0"};
+    while(n>0)
+    {
+      --n;
+      std::cout<<'{';
+      bool first = true;
+      for (int i = 0; i < 64; ++i) {
+        if((1ll << i) & n){
+          if(!first){
+            std::cout << ",";
+          }
+          first = false;
+          std::cout << " " << nums[i];
+        }
+      }
+      std::cout << " }" << std::endl;
+      std::cin >> n;
+    }
+  }
+  int main()
+  { 
+    unsigned long long n;
+    std::cin >> n;
+    print_set(n);
+    return 0;
+  }
