8eb09c5fed45fdbd4de73fe788c8ea93770a403a
Sel <38021042+seleneal1996@users.noreply.github.com>
2021-12-20 09:06:30 -0500

    Update s24_Growling.cpp

--- Aula24:Practica-Mathematics/01Growling Gears/s24_Growling.cpp
+++ Aula24:Practica-Mathematics/01Growling Gears/s24_Growling.cpp
@@ -1,1 +1,33 @@
 //https://open.kattis.com/problems/growlinggears
+#include <bits/stdc++.h>
+void Calculate(){
+  int n_gears;
+  std::cin>>n_gears;
+  int mx_gear=0;
+  double mxRPM=0,mxTorque=-1;
+  for (int gear = 1; gear <= n_gears; gear++)
+  {
+    double a,b,c;
+    std::cin>>a>>b>>c;
+    int rpm = b / (2 * a);
+    int torque = -a*rpm*rpm + b*rpm + c;
+    if (torque > mxTorque+1e-6)
+		{
+			mx_gear = gear;
+			mxRPM = rpm;
+			mxTorque = torque;
+		}
+  }
+  int rounded_max_rpm = mxRPM+ .5;
+  std::cout<<mx_gear<<std::endl;
+}
+int main()
+{
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(NULL);
+  int n;
+  while(n--){
+    Calculate();
+  }
+  return 0;
+}
